c80739e0c71a96278f1dbd2bd492cfca1beda021
Anvarbek Kuvandikov <loving.uz0000@gmail.com>
2024-09-01 18:11:03 +0500

    26. Remove Duplicates from Sorted Array

---
+++ src/26. Remove Duplicates from Sorted Array.kt
@@ -0,0 +1,15 @@
+fun removeDuplicates(nums: IntArray): Int {
+    var cnt = 1
+    for (i in 1..nums.lastIndex) {
+        if (nums[i] != nums[i - 1]) {
+            nums[cnt++] = nums[i]
+        }
+    }
+    return cnt
+}
+fun main() {
+
+    val nums = intArrayOf(0,0,1,1,1,2,2,3,3,4)
+    println(removeDuplicates(nums))
+    println(nums.contentToString())
+}
