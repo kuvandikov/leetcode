315d61ce2d72888bd2780c5be77546690dfe396e
Anvarbek Kuvandikov <loving.uz0000@gmail.com>
2026-08-25 17:41:36 +0500

    solved #2

--- src/1441. Build an Array With Stack Operations.kt
+++ src/1441. Build an Array With Stack Operations.kt
@@ -1,17 +1,16 @@
 fun buildArray(target: IntArray, n: Int): List<String> {
     val stack = mutableListOf<String>()
     var cnt = 0
-    for (i in 1..n) {
-        if (cnt < target.size) {
-            if (target[cnt] == i) {
-                stack.add("Push")
-                cnt++
-            } else {
-                stack.add("Push")
-                stack.add("Pop")
-            }
+
+    for (num in 1..target.last()) {
+        stack.add("Push")
+        if (num == target[cnt]) {
+            cnt++
+        } else {
+            stack.add("Pop")
         }
     }
+
     return stack
 }
 
